a76d63c7fbefd3b3d1c19bd0fb404529a1eb33e6
PongpolR <pongpol.r@astartechs.com>
2025-02-12 10:07:34 +0700

    add localStorage and onSave

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,53 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeModel } from './model/Employee';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular_18_crud';
+  employeeForm: FormGroup = new FormGroup({});
+  employeeObj: EmployeeModel = new EmployeeModel();
+  employeeList: EmployeeModel[] = [];
+
+  constructor() {
+    this.createForm();
+    debugger;
+    const oldData = localStorage.getItem('EmpData');
+    if (oldData != null) {
+      const parseData = JSON.parse(oldData);
+      this.employeeList = parseData;
+    }
+  }
+
+  createForm() {
+    this.employeeForm = new FormGroup({
+      empid: new FormControl(this.employeeObj.empId),
+      name: new FormControl(this.employeeObj.name),
+      city: new FormControl(this.employeeObj.city),
+      address: new FormControl(this.employeeObj.address),
+      contactNo: new FormControl(this.employeeObj.contactNo),
+      emailId: new FormControl(this.employeeObj.emailId),
+      pinCode: new FormControl(this.employeeObj.pinCode),
+      state: new FormControl(this.employeeObj.state),
+    });
+  }
+
+  onSave() {
+    debugger;
+    const oldData = localStorage.getItem('EmpData');
+    if (oldData != null) {
+      const paserData = JSON.parse(oldData);
+      this.employeeForm.controls['empid'].setValue(paserData.length + 1);
+      this.employeeList.unshift(this.employeeForm.value);
+    } else {
+      this.employeeList.unshift(this.employeeForm.value);
+    }
+    localStorage.setItem('EmpData', JSON.stringify(this.employeeList));
+  }
 }
